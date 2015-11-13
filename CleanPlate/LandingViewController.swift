8710fdb1e513e5497fa6dbf392ad8d7194075bf3
Bryan McLellan <bryanmcl@stanford.edu>
2015-11-13 15:25:49 -0800

    imported google maps

--- CleanPlate/LandingViewController.swift
+++ CleanPlate/LandingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class LandingViewController: UIViewController {
 
@@ -21,6 +22,24 @@ class LandingViewController: UIViewController {
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
             self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
         }
+        
+        
+        
+        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+            longitude: 151.20, zoom: 6)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
